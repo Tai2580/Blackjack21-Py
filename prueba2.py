e5391b8c9f2adeaecca2453ba7adcdd05b5ded51
Tai2580 <agarcia@sagradoalcal.edu.ar>
2022-07-05 16:01:30 -0300

    axel

--- prueba2.py
+++
@@ -1,7 +0,0 @@
-def sala():
-        self.ventana2=tk.Tk()
-        self.canvas2=tk.Canvas(self.ventana2, width=450, height=300, background="white")
-        self.canvas2.grid(column=0, row=0)
-        self.ventana2.title("Crear sala")
-        Label (text="Configure su partida", bg="purple", width=450, height=100)
-        self.ventana2.mainloop()
