4aa0f9c80ed319061a3f556eec0e0dc80ae007e5
Tai_AxlGarcia <105065545+Tai2580@users.noreply.github.com>
2022-09-26 14:54:22 -0300

    Algunos cambios

--- Blackjack21.py
+++ Blackjack21.py
@@ -1,9 +1,11 @@
 import tkinter as Blackjack21
 from tkinter import ttk
 import random
+import socket
 from PIL import Image
 
 listacartas = ["img/1C.png", "img/1D.png", "img/1H.png", "img/1S.png", "img/2C.png", "img/2D.png", "img/2H.png", "img/2S.png", "img/3C.png", "img/3D.png", "img/3H.png", "img/3S.png", "img/4C.png", "img/4D.png", "img/4H.png", "img/4S.png", "img/5C.png", "img/5D.png", "img/5H.png", "img/5S.png", "img/6C.png", "img/6D.png", "img/6H.png", "img/6S.png", "img/7C.png", "img/7D.png", "img/7H.png", "img/7S.png", "img/8C.png", "img/8D.png", "img/8H.png", "img/8S.png", "img/9C.png", "img/9D.png", "img/9H.png", "img/9S.png", "img/10C.png", "img/10D.png", "img/10H.png", "img/10S.png", "img/11C.png", "img/11D.png", "img/11H.png", "img/11S.png", "img/12C.png", "img/12D.png", "img/12H.png", "img/12S.png", "img/13C.png", "img/13D.png", "img/13H.png", "img/13S.png"]
+
 
 class Jugador:
     posicion = []
@@ -14,10 +16,11 @@ class Panio:
     manos = []
     mazo = []
     cant_jug = 3
-    crup = 2
+    crup = 1
     posiciones = [[500,400],[500, 20],[900,300],[100,300]]
     lista_cartas = []
     lista_jug = []
+
     
     def __init__(self, cantidad_jugadores=4):
         self.cant_jug = cantidad_jugadores
@@ -36,15 +39,14 @@ class Panio:
                 self.mazo.append('img/' + str(i) + j + '.png')
                 
     def sacar_carta(self):
-        
         self.carta = self.mazo[random.randint(0,len(self.mazo)-1)]
         print(self.carta)
         self.mazo.remove(self.carta)
         return self.carta
     
     def iniciar_juego(self):
-        self.ventana1=Blackjack21.Tk()
-        self.canvas1=Blackjack21.Canvas(self.ventana1, width=1300, height=800, background="darkgreen")
+        self.ventana2=Blackjack21.Tk()
+        self.canvas1=Blackjack21.Canvas(self.ventana2, width=1400, height=1400, background="darkgreen")
         self.canvas1.grid(column=0, row=0)
         for i in range(self.cant_jug):
             self.carta1 = Blackjack21.PhotoImage(file=self.manos[i][0])
@@ -56,22 +58,21 @@ class Panio:
             self.canvas1.create_image(self.posiciones[j][0], self.posiciones[j][1], image=self.lista_cartas[j][0], anchor="nw")
             self.canvas1.create_image(self.posiciones[j][0] + 50, self.posiciones[j][1], image=self.lista_cartas[j][1], anchor="nw")
         
-        self.ventana1.config(width=300, height=200)
+        self.ventana2.config(width=300, height=200)
         boton = ttk.Button(text="Pedir cartas")
         boton.place(x=400, y=550)
-        self.ventana1.mainloop()
+        self.ventana2.mainloop()
         
     def repartir(self, jugador):
         while len(self.manos[jugador]) < 2:
             self.carta = self.sacar_carta()
             if self.carta not in self.manos[jugador]:
                 self.manos[jugador].append(self.carta)
-                
-p1 = Panio(4)
-p1.repartir(0)
-p1.repartir(1)
-p1.repartir(2)
-p1.repartir(3)
 
-print(p1.manos)
-p1.iniciar_juego()
+if __name__ == "__main__":
+    cant_jug = 4
+    p1 = Panio(cant_jug)
+    for i in range(cant_jug):
+        p1.repartir(i)
+    
+    p1.iniciar_juego()
